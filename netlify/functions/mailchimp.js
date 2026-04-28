0301ec9ec5ca3f412445feb53229b053be0e4568
Gina <gena.tognietti@gmail.com>
2026-04-27 19:24:00 -0500

    use env var for api key

--- netlify/functions/mailchimp.js
+++ netlify/functions/mailchimp.js
@@ -2,12 +2,12 @@ exports.handler = async function(event) {
   try {
     const body = JSON.parse(event.body);
     const email = body.email;
-    console.log("Email received:", email);
+    const apiKey = process.env.MAILCHIMP_API_KEY;
     const response = await fetch("https://us14.api.mailchimp.com/3.0/lists/067ce3b1e3/members", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Basic " + Buffer.from("anystring:338937c2f1aff6366db82607ab6f34c1-us14").toString("base64")
+        "Authorization": "Basic " + Buffer.from("anystring:" + apiKey).toString("base64")
       },
       body: JSON.stringify({
         email_address: email,
